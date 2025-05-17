848e092c2eb9e6f5400acfa01d2da9ec3ae78c57
xMasqx <hamzafarooqqureshi@gmail.com>
2025-05-17 22:50:35 +0500

    Fix package configuration for Streamlit deployment

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,22 @@
 PFF Project - A Multi-Themed Financial Machine Learning Application
 """
 
-__version__ = "0.1.0" 
+__version__ = "0.1.0"
+__author__ = "xMasqx"
+__email__ = "hamzafarooqqureshi@gmail.com"
+
+from .themes import (
+    BaseTheme,
+    ZombieTheme,
+    FuturisticTheme,
+    GoTTheme,
+    GamingTheme
+)
+
+__all__ = [
+    'BaseTheme',
+    'ZombieTheme',
+    'FuturisticTheme',
+    'GoTTheme',
+    'GamingTheme'
+] 
